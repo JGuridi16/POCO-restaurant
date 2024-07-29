2c970decd67c116623bfdf11c370cabaa3f2821f
Jacsel Guridi <jguridi@intelutions.net>
2024-07-28 22:51:19 -0400

    Adding more users

--- src/store/data/registeredUsers.js
+++ src/store/data/registeredUsers.js
@@ -6,5 +6,29 @@ export const registeredUsers = [
     email: 'jose_martinez@gmail.com',
     username: 'j_martinez',
     password: '123456',
+  },
+  {
+    id: 2,
+    name: 'Jacsel',
+    lastname: 'Guridi',
+    email: 'jacsel_guridi@gmail.com',
+    username: 'j_guridi',
+    password: '123456',
+  },
+  {
+    id: 3,
+    name: 'Ruby',
+    lastname: 'Rosario',
+    email: 'ruby_rosario@gmail.com',
+    username: 'r_rosario',
+    password: '123456',
+  },
+  {
+    id: 4,
+    name: 'Víctor',
+    lastname: 'Pión',
+    email: 'victor_pion@gmail.com',
+    username: 'v_pion',
+    password: '123456',
   }
 ];
